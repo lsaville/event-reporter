24ecd3cf8dd90ccc853223c8252cd28d0bfa3096
Lee Saville <l.w.saville@gmail.com>
2016-09-08 19:12:51 -0600

    Extract district method to own class, make easier for testing

--- lib/district.rb
+++ lib/district.rb
@@ -2,8 +2,8 @@ require 'open-uri'
 require 'json'
 
 class District
-  def find_district
-    @q.each do |attendee|
+  def find_district(set)
+    set.each do |attendee|
       zipcode = attendee.zipcode
       url = "https://congress.api.sunlightfoundation.com/districts/locate?zip=#{zipcode}&apikey=3321d49851c347e284a0c20c2c73f2d4"
       data = JSON.parse(open(url).read)
--- test/district_test.rb
+++ test/district_test.rb
@@ -5,7 +5,8 @@ require './lib/district'
 class DistrictTest < Minitest::Test
   def test_it_exists
     d = District.new
-
     assert_instance_of District, d
   end
+
+  
 end
